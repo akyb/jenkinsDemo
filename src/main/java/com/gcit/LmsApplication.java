9ff76e7afa9d8f62436651ce1461755387de75fc
akyb <akbar.hosseinkhani@gmail.com>
2017-05-16 20:18:21 -0400

    Update LmsApplication.java

--- src/main/java/com/gcit/LmsApplication.java
+++ src/main/java/com/gcit/LmsApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LmsApplication.class, args);
+		SpringApplication.run(LmsApplication.class, args); 
 	}
 }
