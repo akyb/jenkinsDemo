aeccb24eb2ac23c8e5f0ac1140880486ac0a20ff
akyb <akbar.hosseinkhani@gmail.com>
2017-07-12 19:01:18 -0400

    Update AuthorDao.java

--- src/main/java/com/gcit/dao/AuthorDao.java
+++ src/main/java/com/gcit/dao/AuthorDao.java
@@ -25,8 +25,8 @@ public class AuthorDao {
 		}
 
 		Connection conn = (Connection) DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/library",
-				"root", "root");
+				"jdbc:mysql://gcitsampledb.c0u245fsr0ye.us-east-1.rds.amazonaws.com:3306/library",
+				"gcitsampledb", "gcitsampledb");
 		return conn;
 	}
 
