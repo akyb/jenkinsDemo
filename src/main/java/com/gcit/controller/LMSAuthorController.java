c56b437c30383f4f3ed3b60161a43218b84598ff
akyb <akbar.hosseinkhani@gmail.com>
2018-08-19 17:24:52 -0400

    updated java file

--- src/main/java/com/gcit/controller/LMSAuthorController.java
+++ src/main/java/com/gcit/controller/LMSAuthorController.java
@@ -1,7 +1,7 @@
 package com.gcit.controller;
 
 import java.sql.SQLException;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
