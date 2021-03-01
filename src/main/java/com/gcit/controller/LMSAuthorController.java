5e059cbbc9f43e93417969fc09056122f00abdcf
akbar hosseinkhani <akbarhosseinkhani@Akbars-MacBook-Pro.local>
2021-03-01 18:35:49 -0500

    reducing code

--- src/main/java/com/gcit/controller/LMSAuthorController.java
+++ src/main/java/com/gcit/controller/LMSAuthorController.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gcit.dao.AuthorDao;
+//import com.gcit.dao.AuthorDao;
 import com.gcit.domain.Author;
 
 @RestController
 public class LMSAuthorController {
 
-	@Autowired
-	AuthorDao authorDao = new AuthorDao();
+	// @Autowired
+	// AuthorDao authorDao = new AuthorDao();
 	@RequestMapping("/author/{authorId}")
 	public Author getAuthor(@PathVariable(value = "authorId") int authorId) throws SQLException {
-		Author author = authorDao.getAuthorById(authorId);
+		// Author author = authorDao.getAuthorById(authorId);
+		Author author = new Author();
+		author.setAuthorId(authorId);
+		author.setAuthorName("name "+authorId);
 		return author;
 	}
 }
--- src/main/java/com/gcit/dao/BookDao2.java
+++
@@ -1,5 +0,0 @@
-package com.gcit.dao;
-
-public class BookDao2 {
-
-}
